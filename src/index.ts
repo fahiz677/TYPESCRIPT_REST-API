1be2ad1d4e7280b076bdddff87de59368a496a2c
Muhammed Fahiz <fahizp3@gmail.com>
2023-04-15 06:37:26 +0530

    REST_API
    user register atles connetion file structure

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import dotnev from 'dotenv';
+import mongoose from 'mongoose';
+
+import router from './router';
+
+const app = express();
+
+dotnev.config();
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT ||8080 ;
+
+server.listen(PORT, () => {
+  console.log('Server running on http://localhost:8080/');
+});
+
+const MONGO_URL = 'process.env.CONNECTION_URL'; // DB URI
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
